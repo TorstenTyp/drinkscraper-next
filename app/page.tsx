7a760b6bb50dbaa1b34b9501903f097f4126224f
TorstenTyp <torstenstromberg@protonmail.com>
2025-06-01 15:40:49 +0200

    Before configuring tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,52 @@
-import Image from "next/image";
+import Card from "@/app/ui/components/card";
+import { Cocktail } from "./lib/definitions";
 
-export default function Home() {
+export default async function Home() {
+
+  const cocktails: Cocktail[] = [
+    {
+      ID: 1328,
+      Name: "Harrison Ginsberg’s Martinez",
+      Image: "https://assets-prd.punchdrink.com/wp-content/uploads/2023/03/20105058/Thumb-Martinez-Martini-Cocktail-Reecipe-Harrison-Ginsberg-Overstory-NYC.jpg",
+      Link: "https://punchdrink.com/recipes/harrison-ginsbergs-martinez/",
+      Ingredients: "Sweet vermouth, London dry gin, Old tom gin, Maraschino liqueur, Angostura bitters",
+      Source: "Punchdrink.com"
+    },
+    {
+      ID: 6949,
+      Name: "The Purl",
+      Image: "https://cdn.diffords.com/contrib/stock-images/2017/1/42/2017cb115be0e111e8137617e86d81962a60.jpg",
+      Link: "https://www.diffordsguide.com/cocktails/recipe/2552/the-purl",
+      Ingredients: "Hayman's old tom gin, British bitter ale",
+      Source: "Diffordsguide.com"
+    },
+    {
+      ID: 5877,
+      Name: "Wow",
+      Image: "https://cdn.diffords.com/contrib/stock-images/2022/04/62500f43574da.jpg",
+      Link: "https://www.diffordsguide.com/cocktails/recipe/12054/wow",
+      Ingredients: "Cognac, Calvados straight applejacks, Light white rum, Dubonnetfrench rouge aromatised wine",
+      Source: "Diffordsguide.com"
+    },
+    {
+      ID: 5317,
+      Name: "Singapore Sling",
+      Image: "https://cdn.diffords.com/contrib/stock-images/2015/7/49/20158432d60536445c98d8c03dc77a4ec525.jpg",
+      Link: "https://www.diffordsguide.com/cocktails/recipe/1777/singapore-sling-using-old-tom-gin",
+      Ingredients: "Hayman's old tom gin, Bénédictine d.o.m. liqueur, Cherry heering cherry brandy liqueur, Lemon juice, Angostura aromatic bitters, Orange bitters by angostura, Soda water",
+      Source: "Diffordsguide.com"
+    }
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <h1>Drink Scraper</h1>
+      {cocktails.map(cocktail => (
+        <Card cocktail={{
+          ...cocktail
+        }} key={cocktail.ID} />))
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      }
     </div>
   );
 }
