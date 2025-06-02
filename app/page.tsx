28d0b5f876514581578a5a908351910cf13d04a2
TorstenTyp <torstenstromberg@protonmail.com>
2025-06-02 22:16:09 +0200

    Input bar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Card from "@/app/ui/components/card";
 import { Cocktail } from "./lib/definitions";
 import { mocktails } from '@/app/lib/mockdata'
+import Input from "./ui/components/input";
 
 export default async function Home() {
 
@@ -8,8 +9,11 @@ export default async function Home() {
 
   return (
     <div>
-      <h1 className="font-bold text-4xl p-4">Drink Scraper</h1>
-      <div className="flex flex-wrap grid-cols-fill gap-4 p-4">
+      <header className="bg-white">
+        <h1 className="font-bold text-4xl p-4 pt-8">Drink Scraper</h1>
+        <Input/>
+      </header>
+      <div className="flex flex-wrap grid-cols-fill justify-center gap-4 p-4">
         {cocktails.map(cocktail => (
           <Card cocktail={{
             ...cocktail
