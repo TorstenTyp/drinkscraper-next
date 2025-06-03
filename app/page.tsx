e862d7ce57002b4967eec412ab85f1d543e49b0e
TorstenTyp <torstenstromberg@protonmail.com>
2025-06-03 23:09:42 +0200

    Connected the database

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import Card from "@/app/ui/components/card";
 import { Cocktail } from "./lib/definitions";
 import { mocktails } from '@/app/lib/mockdata'
 import Input from "./ui/components/input";
+import { getTwentyCocktails } from "./lib/data";
 
 export default async function Home() {
 
-  const cocktails: Cocktail[] = mocktails;
+  const cocktails: Cocktail[] = await getTwentyCocktails()
 
   return (
     <div>
       <header className="bg-white">
-        <h1 className="font-bold text-4xl p-4 pt-8">Drink Scraper</h1>
+        <h1 className="font-bold text-4xl p-4 pl-8 pt-8">Drink Scraper</h1>
         <Input/>
       </header>
       <div className="flex flex-wrap grid-cols-fill justify-center gap-4 p-4">
