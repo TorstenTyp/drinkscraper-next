bb5de9e0ce7e07ab3d57071ac1d753724e585902
TorstenTyp <strombergtorsten@gmail.com>
2025-11-18 22:48:29 +0100

    Allow new images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['assets-prd.punchdrink.com', 'cdn.diffords.com', 'cdn.shortpixel.ai', 'www.liquor.com', 'iba-world.com'],
+        domains: ['assets-prd.punchdrink.com', 'cdn.diffords.com', 'cdn.shortpixel.ai', 'www.liquor.com', 'iba-world.com', 'cdn.diffordsguide.com'],
     },
 };
 
