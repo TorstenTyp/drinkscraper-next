d369b59add845b55a79eca892328d25b976e29ba
TorstenTyp <strombergtorsten@gmail.com>
2025-11-23 17:42:12 +0100

    Prevent auto-zoom on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
 import type { Metadata } from "next";
 import { merriweather } from '@/app/ui/fonts';
+import type { Viewport } from 'next'
 import "@/app/ui/globals.css";
 
 export const metadata: Metadata = {
   title: "Drink Scraper",
   description: "Cocktails gathered from all over the internet",
 };
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
@@ -14,8 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-      className={`${merriweather.className} antialiased bg-slate-100`}
+      <body
+        className={`${merriweather.className} antialiased bg-slate-100`}
       >{children}</body>
     </html>
   );
